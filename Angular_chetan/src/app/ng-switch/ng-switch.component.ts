2620662bf778faf56bdcc6d7b5ea0375df86d5c5
Officialakshat <165244500+Officialakshat@users.noreply.github.com>
2025-08-14 19:05:09 +0530

    update

--- Angular_chetan/src/app/ng-switch/ng-switch.component.ts
+++ Angular_chetan/src/app/ng-switch/ng-switch.component.ts
@@ -1,11 +1,16 @@
+import { CommonModule, NgSwitch } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ng-switch',
-  imports: [],
+  imports: [NgSwitch, CommonModule],
   templateUrl: './ng-switch.component.html',
   styleUrl: './ng-switch.component.css'
 })
 export class NgSwitchComponent {
+   grade : number = 0;
 
+   set(x : number){
+     this.grade = x;
+   }
 }
