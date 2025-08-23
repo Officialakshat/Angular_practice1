917ca115a4df71534827a30bcb253c7c5a4d4c9d
Officialakshat <165244500+Officialakshat@users.noreply.github.com>
2025-08-23 23:18:26 +0530

    new

--- Angular_chetan/src/app/reactive-form/reactive-form.component.ts
+++ Angular_chetan/src/app/reactive-form/reactive-form.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.css'
 })
 export class ReactiveFormComponent {
+   reactiveForm : FormGroup;
 
+   constructor(){
+    this.reactiveForm = new FormGroup({
+      firstName : new FormControl(""),
+      lastName : new FormControl(""),
+      email : new FormControl(""),
+      password : new FormControl(""),
+      isChecked : new FormControl(),
+      address : new FormGroup({
+        city : new FormControl(""),
+        Street : new FormControl(""),
+        pincode : new FormControl(),
+      })
+    })
+   }
+
+   onSubmit(){
+    console.log(this.reactiveForm.value);
+    
+   }
 }
