c589b55d635f54c35453c364c4450d8f6dc77a65
Officialakshat <165244500+Officialakshat@users.noreply.github.com>
2025-08-29 13:13:40 +0530

    mew

--- Angular_chetan/src/app/service/student-data.service.ts
+++ Angular_chetan/src/app/service/student-data.service.ts
@@ -6,4 +6,10 @@ import { Injectable } from '@angular/core';
 export class StudentDataService {
 
   constructor() { }
+  userData = [{
+    name: "Akshat",
+    rollNO: 2326289,
+    branch: "B.tech CSE",
+    isPass: true,
+  }]
 }
---
+++ Angular_chetan/src/app/student-data/student-data.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-data',
+  imports: [],
+  templateUrl: './student-data.component.html',
+  styleUrl: './student-data.component.css'
+})
+export class StudentDataComponent {
+
+}
