2368b172cf11a88603e6728ccbb67e19c0ae62e0
Officialakshat <165244500+Officialakshat@users.noreply.github.com>
2025-09-12 21:39:17 +0530

    update

--- Angular_chetan/src/app/update-user/update-user.component.ts
+++ Angular_chetan/src/app/update-user/update-user.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CrudServiceService } from '../service/crud-service.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-update-user',
@@ -6,6 +8,16 @@ import { Component } from '@angular/core';
   templateUrl: './update-user.component.html',
   styleUrl: './update-user.component.css'
 })
-export class UpdateUserComponent {
+export class UpdateUserComponent implements OnInit {
+
+constructor(private crud:CrudServiceService, private activeRoute:ActivatedRoute){}
+
+userId : {
+  uId : number;
+}
+
+ngOnInit(): void {
+    this.userId
+}
 
 }
